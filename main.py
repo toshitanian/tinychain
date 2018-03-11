76f9f3db2bfd3c84e4747099c9a1b0832d01611c
Toshiya Kawasaki <toshiya@abeja.asia>
2018-03-11 23:46:49 +0900

    intiial commit

---
+++ main.py
@@ -0,0 +1,49 @@
+# ref: https://medium.com/crypto-currently/lets-build-the-tiniest-blockchain-e70965a248b
+
+import hashlib as hasher
+import datetime as date
+
+class Block:
+    def __init__(self, index, timestamp, data, previous_hash):
+        self.index = index
+        self.timestamp = timestamp
+        self.data = data
+        self.previous_hash = previous_hash
+        self.hash = self.hash_block()
+
+    def hash_block(self):
+        sha = hasher.sha256()
+        seed = str(self.index) + \
+                   str(self.timestamp) + \
+                   str(self.data) + \
+                   str(self.previous_hash)
+        sha.update(seed.encode('utf-8'))
+        return sha.hexdigest()
+
+    @classmethod
+    def create_genesis_block(cls):
+        return Block(0, date.datetime.now(), "Genesis Block", "0")
+
+    def next_block(self, data):
+        next_index = self.index + 1
+        next_timestamp = date.datetime.now()
+        return Block(next_index, next_timestamp, data, self.hash)
+
+
+if __name__ == '__main__':
+    # Create the blockchain and add the genesis block
+    blockchain = [Block.create_genesis_block()]
+    previous_block = blockchain[0]
+
+    num_of_blocks_to_add = 20
+
+    for i in range(0, num_of_blocks_to_add):
+        block_to_add = previous_block.next_block(f'hi {i}')
+        blockchain.append(block_to_add)
+        previous_block = block_to_add
+        # Tell everyone about it!
+        print("Block #{} has been added to the blockchain!".format(block_to_add.index))
+        print("Hash: {}\n".format(block_to_add.hash))
+
+    for e in blockchain:
+        print(e.data, e.hash, e.previous_hash)
